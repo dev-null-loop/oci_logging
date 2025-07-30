fb4bc7bd4192fc026eabb5563e98f4c6a4ba666a
dev-null-loop <dev.null.loop@gmail.com>
2025-07-30 11:17:16 +0300

    log_group: uncomment validation

--- log_group/main.tf
+++ log_group/main.tf
@@ -10,10 +10,10 @@ resource "oci_logging_log_group" "this" {
   defined_tags   = var.defined_tags
   description    = var.description
   freeform_tags  = var.freeform_tags
-  lifecycle {
-    precondition {
-      condition     = !(length(data.oci_logging_log_groups.these.log_groups) > 0)
-      error_message = "Error: A log group in the compartment already uses this display name. Use a different name."
-    }
-  }
+  # lifecycle {
+  #   precondition {
+  #     condition     = !(length(data.oci_logging_log_groups.these.log_groups) > 0)
+  #     error_message = "Error: A log group in the compartment already uses this display name. Use a different name."
+  #   }
+  # }
 }
